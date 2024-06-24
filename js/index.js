8d2a35e6e33959b7e7b2434f1ee5d27a646d0a5f
Samandar <samandarsharipov202@gmail.com>
2024-06-24 06:20:33 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+"use strict"
+
+// const favYoutuber = prompt("What is your favourite youtuber?")
+// console.log(`http://youtube.com/${favYoutuber}`);
+
+// const user = " Samandar"
+// const job = 'Dispatcher'
+
+// console.log(`My name is ${user}, he is ${job}`);
+
+// let incr = 10
+// let decr = 10
+
+// console.log(++incr);
+// console.log(--decr);
+// console.log(10%6);
+
+// const isAge = true;
+// const isClose = true;
+// const isChecked = true
+
+// console.log(isAge && isClose && !isChecked);
